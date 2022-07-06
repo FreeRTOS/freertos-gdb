e506078ca814e8048cd7d239995a5460c88584aa
System Administrator <root@f84d899204e1.ant.amazon.com>
2022-07-06 11:58:49 -0700

    Fix error when using 'freertos task' between task count increment and task list intialization.

--- freertos.py
+++ freertos.py
@@ -15,7 +15,7 @@ class FreeRTOSList():
 
     def __iter__(self):
         curr_node = self.head
-        while curr_node != self.end_marker.address:
+        while self.uxNumberOfItems > 0 and curr_node != self.end_marker.address:
             tmp_node = curr_node.dereference()
             data = tmp_node['pvOwner'].cast(self.cast_type)
             yield data
@@ -79,6 +79,9 @@ def tasklist_to_rows(tasklist):
 
   return rows
 
+def print_table():
+  return 5
+
 class FreeRTOS(gdb.Command):
     def __init__(self):
         #TODO: choose a gdb.COMMAND_ class for these commands?
@@ -91,10 +94,6 @@ class FreeRTOSTaskInfo(gdb.Command):
     super (FreeRTOSTaskInfo, self).__init__ ("freertos tasks", gdb.COMMAND_NONE)
 
   def invoke (self, arg, from_tty):
-    if gdb.parse_and_eval('uxCurrentNumberOfTasks') == 0:
-      print ("There are currently no tasks. The program may not have created any tasks.")
-      return
-
     table = []
 
     for tasklist_type in TaskLists:
@@ -105,12 +104,13 @@ class FreeRTOSTaskInfo(gdb.Command):
         r = tasklist_val.type.range()
         for i in range(r[0], r[1] + 1):
           rows = tasklist_to_rows(tasklist_val[i])
-          table.append(rows)
+          table.extend(rows)
       else:
         rows = tasklist_to_rows(tasklist_val)
-        table.append(rows)
+        table.extend(rows)
 
     if len(table) == 0:
+      print ("There are currently no tasks. The program may not have created any tasks yet.")
       return
 
     #TODO: some fancier printing function
